63f86e90786b5b193de893191d259563c1b91825
Rebytre <166463229+Rebytre@users.noreply.github.com>
2025-07-25 17:00:40 -0700

    Added Fade

--- index.js
+++ index.js
@@ -7,49 +7,68 @@ By: Rebytre
 
 function homeClicked() {
     //Home Items
-    document.getElementById('headerContainer').style.visibility = 'visible';
-    document.getElementById('personalContainer').style.visibility = 'visible';
+    document.getElementById('homeContainer').style.visibility = 'visible';
+    for (let i = 1; i <= 10; i++) {
+        setTimeout(function() {document.getElementById('homeContainer').style.opacity = i * 0.1;}, i * 50);
+    }
     //Projects Items
     document.getElementById('projectsContainer').style.visibility = 'hidden';
+    document.getElementById('projectsContainer').style.opacity = 0;
     //Socials Items
     document.getElementById('socialsContainer').style.visibility = 'hidden';
+    document.getElementById('socialsContainer').style.opacity = 0;
     //About Me Items
     document.getElementById('aboutMeContainer').style.visibility = 'hidden';
+    document.getElementById('aboutMeContainer').style.opacity = 0;
 }
 
 function projectsClicked() {
     //Home Items
-    document.getElementById('headerContainer').style.visibility = 'hidden';
-    document.getElementById('personalContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.opacity = 0;
     //Projects Items
     document.getElementById('projectsContainer').style.visibility = 'visible';
+    for (let i = 1; i <= 10; i++) {
+        setTimeout(function() {document.getElementById('projectsContainer').style.opacity = i * 0.1;}, i * 50);
+    }
     //Socials Items
     document.getElementById('socialsContainer').style.visibility = 'hidden';
+    document.getElementById('socialsContainer').style.opacity = 0;
     //About Me Items
     document.getElementById('aboutMeContainer').style.visibility = 'hidden';
+    document.getElementById('aboutMeContainer').style.opacity = 0;
 }
 
 function socialsClicked() {
     //Home Items
-    document.getElementById('headerContainer').style.visibility = 'hidden';
-    document.getElementById('personalContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.opacity = 0;
     //Projects Items
     document.getElementById('projectsContainer').style.visibility = 'hidden';
+    document.getElementById('projectsContainer').style.opacity = 0;
     //Socials Items
     document.getElementById('socialsContainer').style.visibility = 'visible';
+    for (let i = 1; i <= 10; i++) {
+        setTimeout(function(){document.getElementById('socialsContainer').style.opacity = i * 0.1;}, i * 50);
+    }
     //About Me Items
     document.getElementById('aboutMeContainer').style.visibility = 'hidden';
+    document.getElementById('aboutMeContainer').style.opacity = 0;
 }
 
 function aboutMeClicked() {
     //Home Items
-    document.getElementById('headerContainer').style.visibility = 'hidden';
-    document.getElementById('personalContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.visibility = 'hidden';
+    document.getElementById('homeContainer').style.opacity = 0;
     //Projects Items
     document.getElementById('projectsContainer').style.visibility = 'hidden';
+    document.getElementById('projectsContainer').style.opacity = 0;
     //Socials Items
     document.getElementById('socialsContainer').style.visibility = 'hidden';
+    document.getElementById('socialsContainer').style.opacity = 0;
     //About Me Items
     document.getElementById('aboutMeContainer').style.visibility = 'visible';
+    for (let i = 1; i <= 10; i++) {
+        setTimeout(function() {document.getElementById('aboutMeContainer').style.opacity = i * 0.1;}, i * 50);
+    }
 }
-
